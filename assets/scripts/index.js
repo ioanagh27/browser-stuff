74fd23944eea58aca7d739417ce041855924947e
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-07-13 11:06:46 +0100

    Add event listeners

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@ document.getElementById("main-title").textContent = "Hi everyone!"
 
 document.querySelector("#main-title").textContent = "Have a good day"
 
-const divs = document.querySelectorAll("div");
+// const divs = document.querySelectorAll("div");
 
 for (let d of divs) {
     d.innerText = "Changed"
@@ -10,4 +10,33 @@ for (let d of divs) {
 
 divs.forEach((d,i ) => {
     d.textContent = `I am at index ${i}`
+})
+
+
+document.getElementById("bitcoin-button").addEventListener("click", () => {
+    console.log("HI!")
+})
+
+
+document.getElementById("bitcoin-button").addEventListener("click", () => {
+    console.log("Hello!")
+})
+
+
+document.getElementById("bitcoin-button").addEventListener("mouseover", () => {
+    console.log("BLA!")
+})
+
+
+document.getElementById("bitcoin-button").addEventListener("click", (e) => {
+    e.target.textContent = "Hmm"
+})
+
+
+const divs = document.querySelectorAll("divs");
+
+divs.forEach((d, i) => {
+    d.addEventListener("click", (e) => {
+      button.textContent = i;
+    })
 })
